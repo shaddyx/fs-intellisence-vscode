eeb781c7adda8ff7087644d5099972e92f10bc73
anatolii.yakushko <shaddyx@gmail.com>
2020-07-04 23:29:31 +0300

    [-] fixed messages

--- src/extension.ts
+++ src/extension.ts
@@ -3,6 +3,11 @@
 import * as vscode from 'vscode';
 import * as cp from 'child_process';
 import * as Commands from './Commands';
+// import * as fs from 'fs';
+
+function escapeShellArg (arg: string) {
+    return `'${arg.replace(/'/g, `'\\''`)}'`;
+}
 
 function runFile(cmd:string, data: string[]|string) : Promise<string> {
 	console.log(`Running command ${cmd} with params: ${data}`);
@@ -12,9 +17,11 @@ function runFile(cmd:string, data: string[]|string) : Promise<string> {
 	} else {
 		args.push(data);
 	}
+	args = args.map(escapeShellArg);
+	const line = vscode.workspace.rootPath + "/.vscode.lng " + args.join(" ");
 	return new Promise((resolve, reject) => {
 		// the resolve / reject functions control the fate of the promise
-		cp.execFile(vscode.workspace.rootPath + "/.vscode.lng", args, (error, stdout, stderr) => {
+		cp.exec(line, (error, stdout, stderr) => {
 			resolve(stdout + stderr);
 		});
 	});
@@ -42,15 +49,22 @@ function createCompletionItem(o: string){
 }
 
 export function activate(context: vscode.ExtensionContext) {
-	
+	// if (!fs.existsSync(vscode.workspace.rootPath + "/.vscode.lng")){
+	// 	vscode.window.showInformationMessage("fs-intellisense: .vscode.lng file not found, switching off...");
+	// 	return;
+	// }
 	return runFile(Commands.INIT, vscode.workspace.rootPath || "")
 	.then(res => {
 		console.log("Init complete: ", res, res.length);
+		if (!res){
+			vscode.window.showErrorMessage("Error initializing fs-intellisense extension, please check .vscode.lng file");
+			return;
+		}
 		let resChunks = res.trim().split(" ");
 		let language = resChunks[1].trim();
 		let divider = resChunks[0].trim();
 		console.log(`lang: ${language} divider: ${divider}`);
-
+		
 		const provider = vscode.languages.registerCompletionItemProvider(
 			language,
 			{
@@ -95,6 +109,9 @@ export function activate(context: vscode.ExtensionContext) {
 		});
 		context.subscriptions.push(provider);
 		context.subscriptions.push(definitionProvider);
+	})
+	.catch(err => {
+		vscode.window.showErrorMessage("Error initializing fs-intellisense extension: " + err);
 	});
 }
 
